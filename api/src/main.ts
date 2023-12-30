e5f31ce9abe16b29cf5ddc26c0dc6ef2036c6afd
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-12-29 22:42:37 -0300

    fix: remove env import

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import "dotenv/config";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
