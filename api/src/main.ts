25e0dd68572fb102cfe2e3720a51ba4caefac074
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-01-13 13:52:20 -0300

    feat: enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: 'https://fincheck-cloud.vercel.app/' });
 
   await app.listen(3000);
 }
