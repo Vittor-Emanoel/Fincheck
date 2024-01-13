9b029a46c72c6561bc574b8de0b7de5550dace7d
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-01-12 23:57:17 -0300

    fix: remove import

--- fe/src/app/services/usersService/me.ts
+++ fe/src/app/services/usersService/me.ts
@@ -1,8 +1,7 @@
-import { UserData } from "../../entities/User";
 import { httpClient } from "../httpClient";
 
 export async function me() {
-  const { data } = await httpClient.get<UserData>("/users/me");
+  const { data } = await httpClient.get("/users/me");
 
   return data;
 }
