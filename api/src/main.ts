4f298947a951998d59861155e2278fd2c504af8f
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-01-13 15:07:57 -0300

    fix: cors enable

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ origin: 'https://fincheck-cloud.vercel.app/' });
+
+  app.enableCors({ origin: 'https://fincheck-cloud.vercel.app' });
 
   await app.listen(3000);
 }
