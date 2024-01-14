25c670701c1e025f2c33d70015b97e12eed7081f
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-01-14 12:08:27 -0300

    fix: wild card cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'https://fincheck-cloud.vercel.app',
+    origin: '*',
   });
 
   await app.listen(3333);
